e7309967d9135b663a218357896e521761bb4e91
julzajules <juliamuiruri1999@gmail.com>
2022-02-15 15:15:01 +0300

    JavaScript conditional operator

---
+++ JavaScriptFundamentals/conditionalOperator.js
@@ -0,0 +1,10 @@
+
+ // let age = prompt('What is your age?', 0);
+ let age = 2;
+ let response = (age < 3) ? console.log('Infant!') : console.log('Not infant'); //if age is less that 3, alert infact, else alert not infant
+
+
+ // let ans = prompt('What is the official name of JavaScript?', 'js');
+ let ans = 'ECMAScript';
+ let result = (ans != 'ECMAScript') ? 'You do not know? It is ECMAScript' : 'Right';
+ console.log(result);
